32d5097c25ad804920a18be07587d7b0856a0229
EdwardAndress <eddie_andress@hotmail.com>
2018-05-11 15:09:28 +0100

    Mission reports mean median and number of repos

--- lib/mission.rb
+++ lib/mission.rb
@@ -9,9 +9,31 @@ class Mission
   end
 
   def report
-    targets.map do |target|
+    header = ['GitHub ID', 'Mean', 'Median', 'Repos']
+    data = targets.map do |target|
       spy = @spy.new(target: target, from: @date_from, to: @date_to)
-      [ spy.report.keys, spy.report.values ].flatten
+      scores = spy.report.values.flatten
+
+      [ target, mean(scores), median(scores), scores.length ]
+    end
+
+    data.prepend(header)
+  end
+
+  def mean(values)
+    result = values.sum / values.length.to_f
+    return sprintf('%.2f', result)
+  end
+
+  def median(values)
+    if values.length % 2 == 1
+      result = values.sort[values.length/2]
+      return sprintf('%.2f', result)
+    else
+      lower = values.sort[values.length/2 - 1].to_f
+      upper = values.sort[values.length/2].to_f
+      result = mean([lower, upper])
+      return sprintf('%.2f', result)
     end
   end
 end
--- spec/mission_spec.rb
+++ spec/mission_spec.rb
@@ -3,7 +3,7 @@ require_relative '../lib/mission.rb'
 RSpec.describe Mission do
 
   let(:spyclass) { double 'Spy', new: spy }
-  let(:spy)      { double 'spy', report: {"EdwardAndress" => [95.01, 100.00]} }
+  let(:spy)      { double 'spy', report: {"EdwardAndress" => [40.00, 70.00, 90.00, 100.00]} }
   subject do
     described_class.new(
       targets:['EdwardAndress', 'AndressEdward'],
@@ -26,7 +26,30 @@ RSpec.describe Mission do
     end
 
     it 'returns compiled report' do
-      expect(subject.report).to eq [["EdwardAndress", 95.01, 100.00],["EdwardAndress", 95.01, 100.00]]
+      expect(subject.report).to eq [
+        ['GitHub ID', 'Mean', 'Median', 'Repos' ],
+        ["EdwardAndress", sprintf('%.2f', 75), sprintf('%.2f', 80), 4],
+        ["AndressEdward", sprintf('%.2f', 75), sprintf('%.2f', 80), 4]
+      ]
+    end
+  end
+
+  describe '#mean' do
+    it 'calculates the arithmentic mean' do
+      expect(subject.mean([4,6,8])).to eq sprintf('%.2f', 6)
+    end
+  end
+
+  describe '#median' do
+    context 'with an odd number of values' do
+      it 'caculates the arithmentic median' do
+        expect(subject.median([1,2,3,4,5])).to eq sprintf('%.2f', 3)
+      end
+    end
+    context 'with an even number of values' do
+      it 'caculates the arithmentic median' do
+        expect(subject.median([1,2,3,4,5,6])).to eq sprintf('%.2f', 3.5)
+      end
     end
   end
 end
